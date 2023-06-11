480cf37e7dc92fac0337001b2545843633a251e2
Vedant-Soni <sonivedant66@gmail.com>
2023-06-11 18:37:34 +0530

    IPFS - Practice

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import { IPFS } from './components/IPFS';
+import { SendToPinata } from './components/SendToPinata';
+
+function App() {
+  return (
+    <div className="App">
+      <SendToPinata />
+    </div>
+  );
+}
+
+export default App;
